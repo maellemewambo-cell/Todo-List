27adee9a1e0e8fe3c30f0a470f611a9bec5fdd5b
maellemewambo-cell <maellemewambo@gmail.com>
2026-06-26 01:37:28 +0100

    updated script

--- script.js
+++ script.js
@@ -9,3 +9,20 @@ const addButton = document.querySelector('.btn');
 const deleteButton = document.getElementById('deleteButton');
 
 // Initialize
+// DomContentLoaded cest un event qui se déclenche lorsque le HTML est complètement chargé et analysé, sans attendre que les feuilles de style, images et sous-documents aient fini de charger. Donc cest pour ca que on met addEventListener sur DOMContentLoaded pour que le code à l'intérieur ne s'exécute qu'après que le document soit complètement chargé.
+
+// event.preventDefault() is used to prevent the default action of an event from occurring. In this case, it prevents the default behavior of the Enter key, which is to submit a form or trigger a button click. By calling event.preventDefault(), we can stop that default behavior and handle the Enter key press in our own way, such as adding a task to the list without submitting a form.
+
+Document.addEventListener('DOMContentLoaded', function (){
+    addButton.addEventListener("click", addTask);
+    todoInput.addEventListener('keydown',function(event){
+        if(event.key === 'Enter'){
+            event.preventDefault();
+            addTask();
+        }
+    })
+})
+
+function addTask() {
+
+}
